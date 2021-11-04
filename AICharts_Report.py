caeee7a0ee81e6ccb0c07cb0cf162b04756b9882
Walter Santacruz <wasantac12349@gmail.com>
2021-11-04 14:31:02 -0500

    Word visualiztion red and blue

--- AICharts_Report.py
+++ AICharts_Report.py
@@ -28,7 +28,7 @@ def AIChart_plot_data_treemap(xAAD,title='Treemap',limit=0,saveImg = False):
     colors = [cmap(norm(value)) for value in weights] #Color mapping normalization
     fig = figure(figsize=(10,10)) #Figure size
     ax = fig.add_subplot(111,aspect="equal")
-    ax = squarify.plot(weights,label=tokens,text_kwargs={'color':'white' ,'size': 8},color=colors)
+    ax = squarify.plot(weights,label=tokens,text_kwargs={'color':'#ffffff' ,'size': 8},color=colors)
     plt.axis('off')
     ax.set_title(title)
     if saveImg:
@@ -63,9 +63,38 @@ def AIChart_plot_data_word_graph(xAAD,text_list):
         node_size.append(10+500*v['weight']) #Linear function to adjust node size
         for edge in v['edges']:
             G.add_edge(k,edge)
-    nx.draw(G,with_labels=True, font_weight='bold',node_size=node_size)
+    pos = nx.spring_layout(G, k=0.2, iterations=30)
+    nx.draw(G,pos,with_labels=True,node_size=node_size,font_color="black",font_size=10,edge_color='#999999',width=1,node_color="#ff5757")
     plt.show()
 
+
+def AIChart_plot_data_text(xAAD,text_list):
+    dictionary = xAAD.dictionary
+    fig = plt.figure()
+    contador = 0
+    while len(text_list) > contador*contador:
+        contador = contador + 1
+    next_line = 0
+    next_word = 0
+    for i in range(len(text_list)):
+        if next_word % contador == 0 or next_word > contador:
+            next_line = next_line + 1
+            next_word = 0
+        word = text_list[i]
+        font_size = dictionary[word] * 30
+        if font_size < 8:
+            font_size = 9
+        if font_size < 15:
+            plt.text(next_word,contador - next_line,word,fontsize=font_size,color='blue',fontstyle='oblique',wrap=True)
+        else:
+            plt.text(next_word,contador - next_line,word,fontsize=font_size,color='red',wrap=True)
+        
+        next_word = next_word + len(word)*0.15*font_size/30 + 0.2
+
+        #plt.text(j,contador - i - 1,text_list[indice],fontsize=font_size,wrap=True)
+    plt.axis([1,contador+1,-1,contador+1])
+    plt.axis('off')
+    plt.show()
 #test
 word_dicc = {}
 full_text_list = []
@@ -77,5 +106,6 @@ with open("test.txt","r") as file:
 data = xNB_Classes.xAAD(rd.random(),word_dicc)
 
 
-AIChart_plot_data_treemap(data)
-AIChart_plot_data_word_graph(data,full_text_list)
+#AIChart_plot_data_treemap(data)
+#AIChart_plot_data_word_graph(data,full_text_list)
+AIChart_plot_data_text(data,full_text_list)
